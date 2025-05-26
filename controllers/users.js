51a15b50e95235644fddf8e2c367b48b49b95bf4
Simon Gebord <sgebord39@gmail.com>
2025-05-26 15:53:05 -0400

    added errors.js file

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,16 @@
 const User = require("../models/user");
+const {
+  PAGE_NOT_FOUND,
+  REQUEST_COMPLETED_SUCCESSFULLY,
+  YOUR_DATA_IS_CREATED,
+} = require("../utils/errors");
 
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send(users))
     .catch((err) => {
       console.error(err);
-      return res.status(500).send({ message: err.message });
+      return res.status(SERVER_MALFUNCTION).send({ message: err.message });
     });
 };
 
@@ -13,13 +18,16 @@ const createUser = (req, res) => {
   const { name, avatar } = req.body;
 
   User.create({ name, avatar })
-    .then((user) => res.status(201).send(user))
+    .then((user) => res.status(YOUR_DATA_IS_CREATED).send(user))
     .catch((err) => {
       console.error(err);
       if (err.name === "ValidationError") {
-        return res.status(400).send({ message: err.message });
+        return res
+          .status(BAD_REQUEST_STATUS_CODE)
+          .send({ message: err.message });
+      } else {
+        return res.status(SERVER_MALFUNCTION).send({ message: err.message });
       }
-      return res.status(500).send({ message: err.message });
     });
 };
 
@@ -27,15 +35,17 @@ const getUser = (req, res) => {
   const { userId } = req.params;
 
   User.findById(userId)
-    .then((user) => res.status(200).send(user))
+    .then((user) => res.status(REQUEST_COMPLETED_SUCCESSFULLY).send(user))
     .orFail()
     .catch((err) => {
       if (err.name === "DocumentNotFoundError") {
-        return res.status(404).send({ message: "User not found" });
+        return res.status(PAGE_NOT_FOUND).send({ message: "User not found" });
       } else if (err.name === "CastError") {
-        return res.status(400).send({ message: "Invalid user ID format" });
+        return res
+          .status(BAD_REQUEST_STATUS_CODE)
+          .send({ message: "Invalid user ID format" });
       }
-      return res.status(500).send({ message: err.message });
+      return res.status(SERVER_MALFUNCTION).send({ message: err.message });
     });
 };
 
