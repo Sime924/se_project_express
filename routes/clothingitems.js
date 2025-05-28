714d05e4225026b65bd26fc20ea21a237569c2cc
Simon Gebord <sgebord39@gmail.com>
2025-05-28 14:55:42 -0400

    updated clothingitems.js in routes

--- routes/clothingitems.js
+++ routes/clothingitems.js
@@ -1,8 +1,15 @@
 const router = require("express").Router();
-const { createItem } = require("../controllers/clothingitems");
+const {
+  createItem,
+  likeItem,
+  deleteLike,
+} = require("../controllers/clothingitems");
 
 router.get("/items", () => console.log("GET items"));
 router.post("/", createItem);
 router.delete("/items/:itemId", () => console.log("DELETE items"));
+
+router.put("/items/:itemId/likes", likeItem);
+router.delete("/items/:itemId/likes", deleteLike);
 
 module.exports = router;
