aa41a746c7cd0f46123252a0ddf9cab34de56690
Simon Gebord <sgebord39@gmail.com>
2025-05-30 15:29:15 -0400

    fixed index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
 const userRouter = require("./users");
 const clothingItem = require("./clothingitems");
+const { PAGE_NOT_FOUND } = require("../utils/errors");
 
 router.use("/items", clothingItem);
 router.use("/users", userRouter);
-router.use((req, res) => res.status(404).send({ message: "send 404 ERROR" }));
+router.use((req, res) =>
+  res.status(PAGE_NOT_FOUND).send({ message: "send 404 ERROR" })
+);
 
 module.exports = router;
