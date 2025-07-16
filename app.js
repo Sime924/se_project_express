1269f18aaf11222b163203a1d751c0c7895af114
Simon Gebord <sgebord39@gmail.com>
2025-07-16 15:54:45 -0400

    updated errors.js

--- app.js
+++ app.js
@@ -17,12 +17,12 @@ mongoose
   .catch(console.error);
 
 app.use(express.json());
-app.use(auth);
-app.use(cors());
+
+app.use(cors({ origin: "*" }));
 
 app.post("/signin", login);
 app.post("/signup", createUser);
-
+app.use(auth);
 app.use("/", mainRouter);
 
 app.listen(PORT, () => {
