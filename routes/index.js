cc8404399cce9b6c0bb758bfec3a419155c20976
Simon Gebord <sgebord39@gmail.com>
2025-10-15 16:42:23 -0400

    fixed centralized error handling

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,12 @@ const router = require("express").Router();
 const userRouter = require("./users");
 const clothingItem = require("./clothingitems");
 const { PAGE_NOT_FOUND } = require("../utils/errors");
+const NotFoundErr = require("../errors/not-found-err");
 
 router.use("/items", clothingItem);
 router.use("/users", userRouter);
-router.use((req, res) =>
-  res.status(PAGE_NOT_FOUND).send({ message: "send 404 ERROR" })
-);
+router.use((req, res, next) => {
+  next(new NotFoundErr("Requested resource not found"));
+});
 
 module.exports = router;
